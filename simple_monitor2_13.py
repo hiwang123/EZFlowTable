fb02827c70939c1fbc26c1d3a7a86c586c3d2c38
Hsin Yi Wang <b04705006@ntu.edu.tw>
2017-01-24 18:11:53 +0800

    Add UDP flood classify by ratio. based on "Classification of UDP Traffic for DDoS Detection", report an address with max a/b ratio every 10s. test with normal data, not yet test with attack data.

--- simple_monitor2_13.py
+++ simple_monitor2_13.py
@@ -32,6 +32,10 @@ class SimpleMonitor13(simple_switch2_13.SimpleSwitch2_13):
         self.monitor_thread = hub.spawn(self._monitor)
         self.eth_types = {ether_types.ETH_TYPE_ARP: 'ARP', ether_types.ETH_TYPE_IP: 'IP'}
         self.protos = {in_proto.IPPROTO_TCP: 'TCP', in_proto.IPPROTO_UDP: 'UDP', in_proto.IPPROTO_ICMP: 'ICMP'}
+        self.udp_alpha = [{}, {}]
+        self.udp_beta = [{}, {}]
+        self.udp_f = 1
+        self.udp_cnt = 0
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
@@ -59,6 +63,37 @@ class SimpleMonitor13(simple_switch2_13.SimpleSwitch2_13):
 
         req = parser.OFPFlowStatsRequest(datapath)
         datapath.send_msg(req)
+    
+    def _udp_classify_update(self, stat):
+        src_addr = stat.match['ipv4_src']
+        dst_addr = stat.match['ipv4_dst']
+        f = self.udp_f
+        if src_addr in self.udp_alpha[f]:
+            self.udp_alpha[f][src_addr] += stat.packet_count
+        else:
+            self.udp_alpha[f][src_addr] = stat.packet_count
+        if dst_addr in self.udp_beta[f]:
+            self.udp_beta[f][dst_addr] += stat.packet_count
+        else:
+            self.udp_beta[f][dst_addr] = stat.packet_count
+    
+    def _udp_classify(self):
+        mx_ratio = 0
+        mx_addr = -1
+        f = self.udp_f
+        for addr in self.udp_alpha[f]:
+            cur_alpha = 0 if addr not in self.udp_alpha[f] else self.udp_alpha[f][addr]
+            prev_alpha = 0 if addr not in self.udp_alpha[1-f] else self.udp_alpha[1-f][addr]
+            cur_beta = 0 if addr not in self.udp_beta[f] else self.udp_beta[f][addr]
+            prev_beta = 0 if addr not in self.udp_beta[1-f] else self.udp_beta[1-f][addr]
+            if cur_beta == prev_beta:
+                ratio = float(cur_alpha - prev_alpha)
+            else:
+                ratio = float(cur_alpha - prev_alpha) / (cur_beta - prev_beta)
+            if ratio > mx_ratio:
+                mx_ratio = ratio
+                mx_addr = addr
+        self.logger.info('udp max ratio: %.4f, addr is %s', mx_ratio, mx_addr)
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
@@ -78,16 +113,19 @@ class SimpleMonitor13(simple_switch2_13.SimpleSwitch2_13):
                          '-------- --------')
 
         for stat in sorted([flow for flow in body if flow.priority == 2],
-                           key=lambda flow: (flow.match['eth_type'])):
+                           key=lambda flow: (-flow.packet_count)):
             if stat.match['eth_type'] == ether_types.ETH_TYPE_IP:
                 if stat.match['ip_proto'] == in_proto.IPPROTO_TCP:
+                    '''
                     self.logger.info('%8s %8s %16s %16s %8d %8d %8d %8d %8d',
                                      self.eth_types[stat.match['eth_type']], self.protos[stat.match['ip_proto']],
                                      stat.match['ipv4_src'], stat.match['ipv4_dst'],
                                      stat.match['tcp_src'], stat.match['tcp_dst'],
                                      stat.match['tcp_flags'],
                                      stat.packet_count, stat.byte_count)
+                    '''
                 elif stat.match['ip_proto'] == in_proto.IPPROTO_UDP:
+                    self._udp_classify_update(stat)
                     self.logger.info('%8s %8s %16s %16s %8d %8d %8d %8d %8d',
                                      self.eth_types[stat.match['eth_type']], self.protos[stat.match['ip_proto']],
                                      stat.match['ipv4_src'], stat.match['ipv4_dst'],
@@ -95,17 +133,30 @@ class SimpleMonitor13(simple_switch2_13.SimpleSwitch2_13):
                                      -1,
                                      stat.packet_count, stat.byte_count)
                 elif stat.match['ip_proto'] == in_proto.IPPROTO_ICMP:
+                    '''
                     self.logger.info('%8s %8s %16s %16s %8d %8d %8d %8d %8d',
                                      self.eth_types[stat.match['eth_type']], self.protos[stat.match['ip_proto']],
                                      stat.match['ipv4_src'], stat.match['ipv4_dst'],
                                      -1, -1,
                                      -1,
                                      stat.packet_count, stat.byte_count)
+                    '''
             elif stat.match['eth_type'] == ether_types.ETH_TYPE_ARP:
+                '''
                 self.logger.info('%8s %8s %16s %16s %8d %8d %8d %8d %8d',
                                  self.eth_types[stat.match['eth_type']], None,
                                  stat.match['arp_spa'], stat.match['arp_tpa'],
                                  -1, -1,
                                  -1,
                                  stat.packet_count, stat.byte_count)
+                '''
+        self.udp_cnt += 1
+        if self.udp_cnt == len(self.datapaths):
+            self._udp_classify()
+            self.udp_f = 1 - self.udp_f
+            self.udp_alpha[self.udp_f] = {}
+            self.udp_beta[self.udp_f] = {}
+            self.udp_cnt = 0
+        
+        
 
